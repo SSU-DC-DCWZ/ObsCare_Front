930bf5a0a1a9065caffd2906a076638d26fa3ec8
Park Sejin <49185035+pseeej@users.noreply.github.com>
2021-08-06 02:39:50 +0900

    tried on various ways
    1. 유튜브 영상 재생시키기
    2. 저장된 영상 재생시키기
    지금 생각하고 있는 건 표 형태에 영상 재생시키는 건데,,, 또 생각해보면 굳이 표 형식으로 해야되나 싶기도 하고
    저장된 영상 재생시킬 때도 오류가 뜨긴 한데 잘 수정해보렴

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QPushButton, QToolTip, QMainWindow, QAction, qApp
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import QCoreApplication
+import open_video
 
 class MyApp(QMainWindow):
     def __init__(self):
@@ -17,18 +18,39 @@ class MyApp(QMainWindow):
 
         btn.clicked.connect(QCoreApplication.instance().quit)   # 버튼 누르면 clicked 시그널 생성됨
 
-    def menuBar(self):
-        exitAction = QAction('나가기', self)
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit application')
-        exitAction.triggered.connect(qApp.quit)
+    def table4video(self):
+        # table widget 생성
+        self.tableWidget = QTableWidget()
+        self.tableWidget.setRowCount(2)
+        self.tableWidget.setColumnCount(2)
 
-        self.statusBar()
+        # setEditTriggers()로 테이블의 항목 편집 가능하도록 함
+        self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
-        menubar = self.menuBar()
-        menubar.setNativeMenuBar(False)
-        filemenu = menubar.addMenu('메뉴')
-        filemenu.addAction(exitAction)
+        # 헤더의 크기 조절. 헤더의 폭이 위젯의 폭에 맞춰지도록... 뭐 어쩌구저쩌구
+        self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        for i in range(2):
+            for j in range(2):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(i+j)))
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.tableWidget)
+        self.setLayout(layout)
+
+
+    # def menuBar(self):
+    #     exitAction = QAction('나가기', self)
+    #     exitAction.setShortcut('Ctrl+Q')
+    #     exitAction.setStatusTip('Exit application')
+    #     exitAction.triggered.connect(qApp.quit)
+    #
+    #     self.statusBar()
+    #
+    #     menubar = self.menuBar()
+    #     menubar.setNativeMenuBar(False)
+    #     filemenu = menubar.addMenu('메뉴')
+    #     filemenu.addAction(exitAction)
 
     def initUI(self):
         self.setWindowTitle('My First Application')
@@ -37,7 +59,7 @@ class MyApp(QMainWindow):
         self.resize(1280, 480)   # 위젯의 크기 조절
 
         self.quitButton()
-        self.menuBar()
+        self.table4video()
 
         self.statusBar().showMessage('Ready')   # 창 제일 아래에 ready 떠있음
 
@@ -46,7 +68,7 @@ class MyApp(QMainWindow):
 
         self.show()
 
-if __name__ == '__main__':
+def main():
     app = QApplication(sys.argv)
     ex = MyApp()
     sys.exit(app.exec_())
