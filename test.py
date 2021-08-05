5d64fa46197b28194aff8fdf296ff3052662fdb7
Park Sejin <49185035+pseeej@users.noreply.github.com>
2021-08-06 01:09:05 +0900

    tried on pyqt-designer

--- test.py
+++ test.py
@@ -1,18 +1,52 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QPushButton, QToolTip, QMainWindow, QAction, qApp
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import QCoreApplication
 
-class MyApp(QWidget):
+class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
 
+    def quitButton(self):
+        QToolTip.setFont(QFont('SansSerif', 10))
+        btn = QPushButton('종료', self)
+        btn.setToolTip('Push this button to <b>quit</b> this program')  # 버튼 위에 마우스 갖다대면 힌트 뜸
+        btn.move(1200, 400)
+        btn.resize(btn.sizeHint())
+
+        btn.clicked.connect(QCoreApplication.instance().quit)   # 버튼 누르면 clicked 시그널 생성됨
+
+    def menuBar(self):
+        exitAction = QAction('나가기', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+        filemenu = menubar.addMenu('메뉴')
+        filemenu.addAction(exitAction)
+
     def initUI(self):
         self.setWindowTitle('My First Application')
-        self.move(300, 300)
-        self.resize(400, 200)
+        self.setWindowIcon(QIcon('./img/web.png'))
+        self.move(0, 0) # 위젯을 스크린의 x=300px, y=300px의 위치로 이동
+        self.resize(1280, 480)   # 위젯의 크기 조절
+
+        self.quitButton()
+        self.menuBar()
+
+        self.statusBar().showMessage('Ready')   # 창 제일 아래에 ready 떠있음
+
+        QToolTip.setFont(QFont('SansSerif', 10))
+        self.setToolTip('<b>ObsCure</b> by <b>DCWZ</b>')
+
         self.show()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = myApp()
+    ex = MyApp()
     sys.exit(app.exec_())
