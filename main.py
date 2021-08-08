29510c54833ef4c80b77146e35d436bbdb4267e3
pseeej <jinny8517@naver.com>
2021-08-09 02:09:06 +0900

    I've just done with the video- size
    I'm really sad, upset 정말정말 짜증나 ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ 외않되ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ

--- main.py
+++ main.py
@@ -2,54 +2,29 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import QCoreApplication
-import open_video
+from open_video import *
 
 class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
+        # self.video = Video()
         self.initUI()
-
-    def quitButton(self):
-        QToolTip.setFont(QFont('SansSerif', 10))
-        btn = QPushButton('종료', self)
-        btn.setToolTip('Push this button to <b>quit</b> this program')  # 버튼 위에 마우스 갖다대면 힌트 뜸
-        btn.move(20, 20)
-        btn.resize(btn.sizeHint())
-
-        btn.clicked.connect(QCoreApplication.instance().quit)   # 버튼 누르면 clicked 시그널 생성됨
-
-    def openVideo(self):
-
-        self.label1 = QLabel("안녕 : ")
-        self.label2 = QLabel("암호 : ")
-        self.lineEdit1 = QLineEdit()
-        self.lineEdit2 = QLineEdit()
-
-        layout = QGridLayout()
-
-        layout.addWidget(self.label1, 0, 0)
-        layout.addWidget(self.lineEdit1, 0, 1)
-
-        layout.addWidget(self.label2, 1, 0)
-        layout.addWidget(self.lineEdit2, 1, 1)
-
-        self.setLayout(layout)
 
 
     def initUI(self):
-        self.setWindowTitle('My First Application')
-        self.setWindowIcon(QIcon('./img/web.png'))
-        self.move(0, 0) # 위젯을 스크린의 x=300px, y=300px의 위치로 이동
-        self.resize(960, 480)   # 위젯의 크기 조절
 
-        self.quitButton()
-        open_video.main()
-        self.statusBar().showMessage('Ready')   # 창 제일 아래에 ready 떠있음
+        self.leftMenu = QPushButton("나가기")
+        self.video = Video()
 
-        QToolTip.setFont(QFont('SansSerif', 10))
-        self.setToolTip('<b>ObsCure</b> by <b>DCWZ</b>')
+        layout = QGridLayout()
 
-        self.show()
+        layout.addWidget(self.leftMenu, 0, 0)
+        layout.addWidget(self.video, 0, 1)
+
+        self.setLayout(layout)
+
+        self.video.runVideo()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- open_video.py
+++ open_video.py
@@ -1,12 +1,11 @@
 import cv2
 import numpy as np
+from PyQt5.QtWidgets import *
 
-class VideoBoard():
-    def setCamId(self, id=0):
+class Video(QWidget):
+    def __init__(self, id=0):
+        super().__init__()
         self.CAM_ID = id
-
-    def getCamId(self):
-        return self.CAM_ID
 
     # 검정색 이미지를 생성 단 배율로 더 크게
     # hcout : 높이 배수(2: 세로로 2배)
@@ -28,56 +27,48 @@ class VideoBoard():
     def showMultiImage(self, dst, src, h, w, d, col, row):
         dst[(col * h):(col * h) + h, (row * w):(row * w) + w] = src[0:h, 0:w]
 
-def main():
-    board = VideoBoard()
-    board.setCamId(0)
-    ##### 코드 시작 ####
-    cap = cv2.VideoCapture(board.getCamId())  # 카메라 생성
+    def runVideo(self):
+        self.cap = cv2.VideoCapture(self.CAM_ID)
 
-    if cap.isOpened() == False:  # 카메라 생성 확인
-        print('Can\'t open the CAM(%d)' % (board.getCamId()))
-        exit()
+        if self.cap.isOpened() == False:
+            print("Can't open the CAM(%d)" %(self.CAM_ID))
+            exit()
 
-    # 윈도우 생성 및 사이즈 변경
-    cv2.namedWindow('multiView')
+        cv2.namedWindow('multiView')
 
-    while (True):
-        # 카메라에서 이미지 얻기
-        ret, frame = cap.read()
+        while (True):
+            # 카메라에서 이미지 얻기
+            ret, frame = self.cap.read()
 
-        # 이미지 높이
-        height = frame.shape[0]
-        # 이미지 넓이
-        width = frame.shape[1]
-        # 이미지 색상 크기
-        depth = frame.shape[2]
+            # 이미지 높이
+            height = frame.shape[0]
+            # 이미지 넓이
+            width = frame.shape[1]
+            # 이미지 색상 크기
+            depth = frame.shape[2]
 
+            # 화면에 표시할 이미지 만들기 ( 2 x 2 )
+            dstimage = np.zeros((height * 2, width * 2, depth), np.uint8)
+            color = tuple(reversed((0, 0, 0)))
+            dstimage[:] = color
 
-        # 화면에 표시할 이미지 만들기 ( 2 x 2 )
-        dstimage = np.zeros((height*2, width*2, depth), np.uint8)
-        color = tuple(reversed((0, 0, 0)))
-        dstimage[:] = color
-
-        # 원하는 위치에 복사
-        # 왼쪽 위에 표시(0,0)
-        board.showMultiImage(dstimage, frame, height, width, depth, 0, 0)
-        # 오른쪽 위에 표시(0,1)
-        board.showMultiImage(dstimage, frame, height, width, 1, 0, 1)
+            # 원하는 위치에 복사
+            # 왼쪽 위에 표시(0,0)
+            self.showMultiImage(dstimage, frame, height, width, depth, 0, 0)
+            # 오른쪽 위에 표시(0,1)
+            self.showMultiImage(dstimage, frame, height, width, 1, 0, 1)
             # 왼쪽 아래에 표시(1,0)
-        board.showMultiImage(dstimage, frame, height, width, 1, 1, 0)
-        # 오른쪽 아래에 표시(1,1)
-        board.showMultiImage(dstimage, frame, height, width, 1, 1, 1)
+            self.showMultiImage(dstimage, frame, height, width, 1, 1, 0)
+            # 오른쪽 아래에 표시(1,1)
+            self.showMultiImage(dstimage, frame, height, width, 1, 1, 1)
 
-        # 화면 표시
-        cv2.imshow('multiView', dstimage)
+            # 화면 표시
+            cv2.imshow('multiView', dstimage)
 
-        # 1ms 동안 키입력 대기 ESC키 눌리면 종료
-        if cv2.waitKey(1) == 27:
-            break
+            # 1ms 동안 키입력 대기 ESC키 눌리면 종료
+            if cv2.waitKey(1) == 27:
+                break
 
-    # 윈도우 종료
-    cap.release()
-    cv2.destroyWindow('multiView')
-
-
-main()
+        # 윈도우 종료
+        self.cap.release()
+        cv2.destroyWindow('multiView')
