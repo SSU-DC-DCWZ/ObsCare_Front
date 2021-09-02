b0bf01f3bf5e0a43ee2c47dd52c461899cd3a1da
Park Sejin <49185035+pseeej@users.noreply.github.com>
2021-09-03 01:14:42 +0900

    comments for play_ui

--- play_ui.py
+++ play_ui.py
@@ -13,12 +13,14 @@ form = resource_path("main.ui")
 
 form_class = uic.loadUiType(form)[0]
 
-# 화면을 띄우는데 사용되는 Class 선언
+# WindowClass : main 화면을 띄우는데 사용되는 class
+# form_class : 해당 class에 적용되는 ui
 class WindowClass(QMainWindow, form_class):
     # UI파일 연결
     # 단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야 한다.
     # form_class = uic.loadUiType("main.ui")[0]
 
+    # __init__ : 생성자
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -27,17 +29,21 @@ class WindowClass(QMainWindow, form_class):
         self.action_prev_video.triggered.connect(self.get_find_date)    # 이전 영상 보기 메뉴와 연결
         self.show_alert(1)
 
-    def get_find_date(self):    # 사용자가 입력한 카메라 번호와 날짜로 해당 영상 재생
+    # get_find_date : 입력받은 카메라 번호와 날짜로 영상 재생 위함
+    def get_find_date(self):
+        # 찾고자하는 영상의 정보 입력
         info, ok = QInputDialog.getText(self, 'FindVideo', '카메라 번호 - 날짜를 입력하시오 (1-20210101) : ')
         if ok:
             try :
+                # 입력한 정보에서 cam 번호와 date 추출
                 self.cam, self.date = info.split('-')
             except ValueError:  # 양식에 맞춰 입력하지 않았을 경우
                 QMessageBox.about(self, "Error!", "올바르지 않은 입력입니다.")
                 return self.get_find_date()
 
+            # 입력한 정보를 바탕으로 DB에서 해당 영상의 주소 받아오기 위함
             finddb = DBvideo()
-            get_path = finddb.findrecord(self.cam, self.date)   # 입력한 정보에 대한 영상 주소를 DB에서 가져옴
+            get_path = finddb.findrecord(self.cam, self.date)
 
             if get_path == '':  # 해당 입력에 대한 영상이 존재하지 않을 경우
                 QMessageBox.about(self, "Error!", "해당 입력에 대한 파일이 존재하지 않습니다.")
