aee4be8a6e1dcc775a63dd721a2073d30862b691
Park Sejin <49185035+pseeej@users.noreply.github.com>
2021-09-03 00:53:18 +0900

    comments for play_prev

--- play_prev.py
+++ play_prev.py
@@ -7,25 +7,32 @@ from DB_video.videoDB import *
 import sys
 import datetime
 
+# pixel density를 높이기 위한 선언
 QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
 
-
+# PrevVideo : 이전 영상 재생 위한 class
+# QWidget : pyqt의 widget 이용하여 창 생성 위함
 class PrevVideo(QWidget):
 
+    # __init__ : 생성자
+    # path : 재생하고자 하는 영상의 경로
     def __init__(self, path=None):
         super().__init__()
+        # PyQt Designer를 이용하여 생성한 ui파일 불러오기
         loadUi('prev_player.ui', self)
 
+        # QtWidgets의 QMediaPlayer로 movie player 틀 생성
+        # prev_player.ui의 view를 self.vp로 대입
         self.mp = QMediaPlayer()
         self.vp = self.view
 
-        self.mp.setVideoOutput(self.vp) # QMediaPlayer에 재생하고자 하는 영상 대입하고자
-        self.content = QMediaContent(QUrl.fromLocalFile(path))  # 해당 주소에서 영상 가져오기
-        self.mp.setMedia(self.content)
+        self.mp.setVideoOutput(self.vp) # QMediaPlayer 틀에 video player 대입
+        self.content = QMediaContent(QUrl.fromLocalFile(path))  # 해당 주소에서 영상 불러오기
+        self.mp.setMedia(self.content)  # mp에 영상 대입
         self.mp.play() # 재생 상태를 기본으로 설정
-        self.state.setText("재생")
+        self.state.setText("재생")    # 영상 상태를 label에 출력하도록
 
-        self.play_signal = True
+        self.play_signal = True # 재생 상태 기록 위한 변수
 
         # 영상 길이 계산 위함
         temp_vid = cv2.VideoCapture(path)
@@ -33,10 +40,11 @@ class PrevVideo(QWidget):
         self.vid_length = temp_vid.get(cv2.CAP_PROP_POS_MSEC)
         self.vid_length, self.vid_time = self.calc_time(self.vid_length)
 
+        # 계산한 영상 길이로 재생 바 범위 결정
         self.bar.setRange(0, self.vid_length)
 
-
-        self.bar.sliderMoved.connect(self.barChanged)   # 슬라이더 위치 변동 시
+        # 슬라이더 위치 변동 시
+        self.bar.sliderMoved.connect(self.barChanged)
 
         # 각 버튼별 함수 연결
         self.btn_play_pause.clicked.connect(self.clickPlayPause)
@@ -49,14 +57,17 @@ class PrevVideo(QWidget):
         self.mp.positionChanged.connect(self.positionChanged)
 
     def change_file(self):  # 다른 영상으로 변경하고자 하는 경우
+        # 영상 정보 입력
         info, ok = QInputDialog.getText(self, 'FindVideo', '카메라 번호 - 날짜를 입력하시오 (1-20210101) : ')
         if ok:
             try:
+                # 입력 받은 info를 카메라 번호와 날짜로 분리
                 self.cam, self.date = info.split('-')
             except ValueError:  # 양식에 맞춰 입력하지 않았을 경우
                 QMessageBox.about(self, "Error!", "올바르지 않은 입력입니다.")
-                return self.change_file()
+                return self.change_file()   # 재입력 요청
 
+            # DBvideo 객체 생성
             finddb = DBvideo()
             get_path = finddb.findrecord(self.cam, self.date)   # 사용자가 입력한 정보에 대한 영상 주소를 db에서 추출
 
@@ -65,12 +76,15 @@ class PrevVideo(QWidget):
                 return self.change_file()
 
             # 찾은 영상 재생
-            self.hide()
-            self.PrevVideo = PrevVideo(get_path)
-            self.PrevVideo.show()
+            self.hide() # 현재 열려있는 창 숨기기
+            self.PrevVideo = PrevVideo(get_path)    # 객체 생성하고 인자로 받은 path의 영상 재생하도록
+            self.PrevVideo.show()   # PrevVideo 창 띄우기
 
-
-    def calc_time(self, sec):   # sec를 시간으로 변경
+            
+    # calc_time : sec 단위의 영상 길이 계산 위함
+    # sec : 영상 길이
+    # int(intoS), res : 실제 영상 길이를 int type으로 바꾼 값과 시:분:초 형태로 만든 str 반환
+    def calc_time(self, sec): 
         sec = sec / 60 // 0.1 * 6
         intoS = sec
 
@@ -84,15 +98,19 @@ class PrevVideo(QWidget):
 
         return int(intoS), res
 
-
-    def clickPlayPause(self):   # 재생 버튼
+    # clickPlayPause : 재생 버튼에 연결된 함수. 영상 재생과 일시정지 기능
+    def clickPlayPause(self):
+        # 영상이 재생중일 때 play/pause 버튼 클릭 시 일시정지
         if self.play_signal == True:
             self.mp.pause()
             self.play_signal = False
+        # 영상이 일시정지 상태일 때 play/pause 버튼 클릭 시 영상 재생
         else:
             self.mp.play()
             self.play_signal = True
 
+    # mediaStateChanged : 재생 상태를 QLabel로써 기록 위함
+    # state : 현재 영상의 재생 상태
     def mediaStateChanged(self, state): # 재생 상태
         if state == QMediaPlayer.StoppedState:
             msg = '정지'
@@ -102,22 +120,28 @@ class PrevVideo(QWidget):
             msg = '일시정지'
         self.updateState(msg)
 
-    def durationChanged(self, duration):    # 영상 길이에 따른 bar 범위 조정
+    # durationChanged : 영상 길이에 따른 bar 범위 조정
+    # duration : 영상 길이
+    def durationChanged(self, duration):
         self.bar.setRange(0, duration)
 
-    def positionChanged(self, pos): # 사용자의 영상 재생 위치 변경에 따른 함수
+    # positionChanged : 사용자의 영상 재생 위치 변경에 따른 함수
+    # pos : 현재 재생 위치
+    def positionChanged(self, pos):
         self.bar.setValue(pos)
         self.updatePos(pos)
 
-    
-    def barChanged(self, pos):  # 재생 상태 슬라이더 움직였을 경우 호출
+    # barChanged : 재생 상태 슬라이더 움직였을 경우 현재 위치 이동
+    def barChanged(self, pos):
         self.mp.setPosition(pos)
 
-    def updateState(self, msg): # stateChanged signal 발생 시 재생 상태 변경하여 출력
+    # stateChanged Signal 발생시 재생 상태 변경하여 출력
+    def updateState(self, msg):
         self.state.setText(msg)
 
-
-    def updateBar(self, duration):  # 동영상 file 변경될 때마다 현재 동영상의 재생시간으로 슬라이더 범위 초기화
+    # updateBar : 동영상 file 변경될 때마다 현재 동영상의 재생시간으로 슬라이더 범위 초기화
+    # duration : 동영상 길이
+    def updateBar(self, duration):
         self.bar.setRange(0, duration)
         self.bar.setSingleStep(int(duration / 10))
         self.bar.setPageStep(int(duration / 10))
@@ -127,8 +151,9 @@ class PrevVideo(QWidget):
         idx = stime.rfind('.')
         self.duration = stime[:idx]
 
-
-    def updatePos(self, pos):   # 현재 재생 위치 전달 위함
+    # updatePos : 현재 재생 위치 기록 및 update
+    # pos : 현재 재생 위치
+    def updatePos(self, pos):
         self.bar.setValue(pos)
         td = datetime.timedelta(milliseconds=pos)
         stime = str(td)
